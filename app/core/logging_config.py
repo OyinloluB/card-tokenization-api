d4380b7bed2431cf7ef754bb485426330e558918
OyinloluB <oyinloluwab@gmail.com>
2025-05-17 23:51:12 -0400

    fixed setup_logging format, added nginx to docker yml

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -27,28 +27,28 @@ class JSONFormatter(logging.Formatter):
         # converts to json string
         return json.dumps(logobj)
     
-    def setup_logging(log_level="INFO"):
-        """configure logging for the application."""
-        
-        # convert string log level to numeric value
-        numeric_level = getattr(logging, log_level.upper(), None)
-        if not isinstance(numeric_level, int):
-            numeric_level = logging.INFO
-        
-        # root logger configuration
-        root_logger = logging.getLogger()
-        root_logger.setLevel(numeric_level)
-        
-        # remove existing handlers
-        for handler in root_logger.handlers[:]:
-            root_logger.removeHandler(handler)
-        
-        # console handler
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(JSONFormatter())
-        root_logger.addHandler(handler)
-        
-        # set lower level for uvicorn.access to avoid double-logging
-        logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
-        
-        return root_logger
+def setup_logging(log_level="INFO"):
+    """configure logging for the application."""
+    
+    # convert string log level to numeric value
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        numeric_level = logging.INFO
+    
+    # root logger configuration
+    root_logger = logging.getLogger()
+    root_logger.setLevel(numeric_level)
+    
+    # remove existing handlers
+    for handler in root_logger.handlers[:]:
+        root_logger.removeHandler(handler)
+    
+    # console handler
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(JSONFormatter())
+    root_logger.addHandler(handler)
+    
+    # set lower level for uvicorn.access to avoid double-logging
+    logging.getLogger("uvicorn.access").setLevel(logging.WARNING)
+    
+    return root_logger
